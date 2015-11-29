b17f691f129490177c10083e277ad0aea3709c9a
Coolthulhu <Coolthulhu@gmail.com>
2015-11-29 15:49:51 +0100

    Tłumaczenie, dokumentacja i warningi

--- prolog/symdiff.pl
+++ prolog/symdiff.pl
@@ -1,231 +1,230 @@
-  /*---------------------------------------------------------*/
-  /*                                                         */
-  /*                       'SYMDIFF'                         */
-  /*                                                         */
-  /*  Symbolic Differentiation and Algebraic Simplification  */
-  /*                                                         */
-  /*    The declarative semantics of Prolog are a powerful   */
-  /*  tool for problems involving symbolic manipulation.     */
-  /*    In this example the rules for differentiation are    */
-  /*  expressed as a set of clauses, where, provided that    */
-  /*  the head matches the expression in question, the body  */
-  /*  furnishes the appropriate result. This result in turn  */
-  /*  is passed to the simplifying clauses which operate on  */
-  /*  the same principle, but obviously correspond to a      */
-  /*  different set of (simplifying) rules.                  */
-  /*                                                         */
-  /*---------------------------------------------------------*/
-
-
-/*  This is the main predicate. */         
-
-symdiff :-
-   nl,
-   write('SYMDIFF : Symbolic Differentiation and '),
-   write('Algebraic Simplification. Version 1.0'), nl,
-   symdiff1.
-
-
-  /*  symdiff1 controls the main processing; It prompts  */
-  /*  the user for data, differentiates the input,       */
-  /*  simplifies the result and then displays it on      */
-  /*  the terminal.                                      */
-
-symdiff1  :-
-   write('Enter a symbolic expression, or "stop" '),
-   read(Exp),
+﻿
+symdiff  :-
+   write( 'Wpisz wyrazenie albo "q" ' ),
+   read( Exp ),
    process( Exp ).
 
 
-process( stop ) :- !.
+process( q ) :- !.
 
+/* Zapytaj o wyrażenie i zmienną do różniczkowania, zróżniczkuj wyrażenie po tej zmiennej, uprość wynik i wypisz, potem zapętl */
 process( Exp ) :-
-   write('Differentiate w.r.t. :'),
-   read(Wrt),
-   d( Exp, Wrt, Diff ),
-   simp( Diff, Sdiff ), nl,
-   write('Differential w.r.t '), write(Wrt),
-   write(' is '),
-   write(Sdiff), nl, nl,
-   symdiff1.
+   write('Rozniczkuj po :'),
+   read( Wrt ),
+   diff( Exp, Wrt, Diff ),
+   simplify( Diff, Sdiff ), nl,
+   write( 'Pochodna ' ), write( Wrt ),
+   write( ' to ' ),
+   write( Sdiff ), nl, nl,
+   symdiff.
 
+/* Różniczkowanie x po x to zawsze 1 */
+diff( X, X, 1 ):- !.
 
+/* Stała (nie będąca x) różniczkowana po x == 0 */
+diff( C, _X, 0 ):- atomic(C).
 
-  /*  The following clauses constitute the differentiation   */
-  /*  rules, some of which are recursive. Note too the use   */
-  /*  of the cut which ensures that once a special case has  */
-  /*  been identified, backtracking will not attempt to find */
-  /*  an alternative solution.                               */
+/* d/dx U+V, gdzie U i V dowolne */
+diff( U+V, X, A+B ):-
+   diff( U, X, A ),
+   diff( V, X, B ).
 
-d( X, X, 1 ):- !.                  /* d(X) w.r.t. X is 1      */
+/* d/dx U-V, gdzie U i V dowolne */
+diff( U-V, X, A-B ):-
+   diff( U, X, A ),
+   diff( V, X, B ).
 
-d( C, X, 0 ):- atomic(C).          /* If C is a constant then */
-                                   /* d(C)/dX is 0            */
+/* d/dx C*U, gdzie U dowolne, C - stała albo symbol inny niż X */
+diff( C*U, X, C*A ):-
+   atomic(C),
+   C \= X,
+   diff( U, X, A ), !.
 
-d( U+V, X, A+B ):-                 /* d(U+V)/dX = A+B where   */
-   d( U, X, A ),                   /* A = d(U)/dX and         */
-   d( V, X, B ).                   /* B = d(V)/dX             */
+/* d/dx U*V, gdzie U i V dowolne nie "złapane" powyżej */
+diff( U*V, X, B*U+A*V ):-
+   diff( U, X, A ),
+   diff( V, X, B ).
 
-d( U-V, X, A-B ):-                 /* d(U-V)/dX = A-B where   */
-   d( U, X, A ),                   /* A = d(U)/dX and         */
-   d( V, X, B ).                   /* B = d(V)/dX             */
+/* d/dx U/V, gdzie U i V dowolne */
+/* Można by tu złapać dzielenie przez 0 */
+diff( U/V, X, (A*V-B*U)/(V*V) ):-
+   diff( U, X, A ),
+   diff( V, X, B ).
 
-d( C*U, X, C*A ):-               /* d(C*U)/dX = C*A where     */
-   atomic(C),                    /* C is a number or variable */
-   C \= X,                       /* not equal to X and        */
-   d( U, X, A ), !.              /* A = d(U)/dX               */
+/* d/dx U^C, gdzie U dowolne, C - stała albo symbol inny niż X */
+diff( U^C, X, C*A*U^(C-1) ):-
+   atomic(C),
+   C\=X,
+   diff( U, X, A ).
 
-d( U*V, X, B*U+A*V ):-           /* d(U*V)/dX = B*U+A*V where */
-   d( U, X, A ),                 /* A = d(U)/dX and           */
-   d( V, X, B ).                 /* B = d(V)/dX               */
+/* j.w., ale C z minusem */
+diff( U^C, X, C*A*U^(C-1) ):-
+   C = -(C1), atomic(C1),
+   C1\=X,
+   diff( U, X, A ).
 
-d( U/V, X, (A*V-B*U)/(V*V) ):- /* d(U/V)/dX = (A*V-B*U)/(V*V) */
-   d( U, X, A),                /* where A = d(U)/dX and       */
-   d( V, X, B).                /*       B = d(V)/dX           */
+/* Szczególny przypadek: x^x */
+diff( U^U, X, (log(U)+1)*U^U ):-
+   diff( U, X, _A ).
 
-d( U^C, X, C*A*U^(C-1) ):-       /* d(U^C)/dX = C*A*U^(C-1)   */
-   atomic(C),                    /* where C is a number or    */
-   C\=X,                         /* variable not equal to X   */
-   d( U, X, A ).                 /* and d(U)/dX = A           */
+/* Wybrane funkcje jednoargumentowe, dla dowolnego argumentu */
+diff( sin(W), X, Z*cos(W) ):-
+   diff( W, X, Z ).
+diff( cos(W), X, -(Z*sin(W)) ):-
+   diff( W, X, Z ).
+diff( exp(W), X, Z*exp(W) ):-
+   diff( W, X, Z ).
+diff( log(W), X, Z/W ):-
+   diff( W, X, Z ).
 
-d( U^C, X, C*A*U^(C-1) ):-       /* d(U^C)/dX = C*A*U^(C-1)   */
-   C = -(C1), atomic(C1),        /* where C is a negated number or  */
-   C1\=X,                        /* variable not equal to X   */
-   d( U, X, A ).                 /* and d(U)/dX = A           */
+/* Upraszczanie wyrażeń */
+/* Tylko sąsiednich...  */
 
-d( sin(W), X, Z*cos(W) ):-       /* d(sin(W))/dX = Z*cos(W)   */
-   d( W, X, Z).                  /* where Z = d(W)/dX         */
-
-d( exp(W), X, Z*exp(W) ):-       /* d(exp(W))/dX = Z*exp(W)   */
-   d( W, X, Z).                  /* where Z = d(W)/dX         */
-
-d( log(W), X, Z/W ):-            /* d(log(W))/dX = Z/W        */
-   d( W, X, Z).                  /* where Z = d(W)/dX         */
-
-d( cos(W), X, -(Z*sin(W)) ):-    /* d(cos(W))/dX = Z*sin(W)   */
-   d( W, X, Z).                  /* where Z = d(W)/dX         */
-
-
-
-  /*  The following clauses are rules for the simplification  */
-  /*  of the result of the differentiation process. For       */
-  /*  example, multiples of zero are dropped, terms gathered  */
-  /*  together where possible and factorisation is attempted. */
-  /*  (It should be noted that in this program only adjacent  */
-  /*  terms are candidates for simplification.)               */
-
-simp( X, X ):-          /* an atom or number is simplified */
+/* Atom jest już uproszczony */
+simplify( X, X ):-
    atomic(X), !.
 
-simp( X+0, Y ):-        /* terms of value zero are dropped  */
-   simp( X, Y ).
+/* Odrzucamy dodane zero z obu stron */
+simplify( X+0, Y ):-
+   simplify( X, Y ).
+simplify( 0+X, Y ):-
+   simplify( X, Y ).
 
-simp( 0+X, Y ):-        /* terms of value zero are dropped  */
-   simp( X, Y ).
+/* j.w. dla odejmowania */
+simplify( X-0, Y ):-
+   simplify( X, Y ).
+simplify( 0-X, -(Y) ):-
+   simplify( X, Y ).
 
-simp( X-0, Y ):-        /* terms of value zero are dropped  */
-   simp( X, Y ).
-
-simp( 0-X, -(Y) ):-     /* terms of value zero are dropped  */
-   simp( X, Y ).
-
-simp( A+B, C ):-        /* sum numbers */
+/* Suma stałych */
+simplify( A+B, C ):-
    numeric(A),
    numeric(B),
    C is A+B.
 
-simp( A-A, 0 ).         /* evaluate differences */
+/* x-x to 0 dla dowolnego x (w tym wyrażeń) */
+simplify( A-A, 0 ).
 
-simp( A-B, C ):-        /* evaluate differences */
+/* Różnica stałych */
+simplify( A-B, C ):-
    numeric(A),
    numeric(B),
    C is A-B.
 
-simp( X*0, 0 ).         /* multiples of zero are zero */
+/* Uproszczenia mnożenia i dzielenia */
+simplify( _X*0, 0 ).
+simplify( 0*_X, 0 ).
 
-simp( 0*X, 0 ).         /* multiples of zero are zero */
+/* Dzielenie zera (tu można by wywalić błąd przy dzieleniu przez 0) */
+simplify( 0/_X, 0 ).
 
-simp( 0/X, 0 ).         /* numerators evaluating to zero are zero */
 
-simp( X*1, X ).         /* one is the identity for multiplication */
+simplify( X*1, X ).
+simplify( 1*X, X ).
+simplify( X/1, X ).
+simplify( X/X, 1 ) :- !.
+simplify( X^1, X ) :- !.
+simplify( _X^0, 1 ) :- !.
 
-simp( 1*X, X ).         /* one is the identity for multiplication */
+/* Zamiana na potęgowanie */
+simplify( X*X, X^2 ) :- !.
 
-simp( X/1, X ).         /* divisors of one evaluate to the numerator */
+/* "Zwijanie" do istniejącej potęgi */
+simplify( X*X^A, Y ) :-
+   simplify( X^(A+1), Y ), !.
+simplify( X^A*X, Y ) :-
+   simplify( X^(A+1), Y ), !.
 
-simp( X/X, 1 ) :- !.
-
-simp( X^1, X ) :- !.
-
-simp( X^0, 1 ) :- !.
-
-simp( X*X, X^2 ) :- !.
-
-simp( X*X^A, Y ) :-
-   simp( X^(A+1), Y ), !.
-
-simp( X^A*X, Y ) :-
-   simp( X^(A+1), Y ), !.
-
-simp( A*B, X ) :-       /* do evaluation if both are numbers  */
+/* Mnożenie stałych */
+simplify( A*B, X ) :-
    numeric( A ),
    numeric( B ),
    X is A*B.
 
-simp( A*X+B*X, Z ):-    /* factorisation and recursive */
-   A\=X, B\=X,          /* simplification              */
-   simp( (A+B)*X, Z ).
+/* A*X+B*X = (A+B)*X (chyba że A == X albo B == X) */
+simplify( A*X+B*X, Z ):-
+   A\=X, B\=X,
+   simplify( (A+B)*X, Z ).
 
-simp( (A+B)*(A-B), X^2-Y^2 ):-   /* difference of two squares */
-   simp( A, X ),
-   simp( B, Y ).
+/* Wzór skróconego mnożenia: (A+B)*(A-B) = A^2 + B^2 */
+simplify( (A+B)*(A-B), X^2-Y^2 ):-
+   simplify( A, X ),
+   simplify( B, Y ).
 
-simp( X^A/X^B, X^C ):-     /* quotient of numeric powers of X */
+/* X^A/X^B = X^(A-B) */
+simplify( X^A/X^B, X^C ):-
    numeric(A), numeric(B),
    C is A-B.
 
-simp( A/B, X ) :-       /* do evaluation if both are numbers  */
+/* Dzielenie stałych */
+simplify( A/B, X ) :-
    numeric( A ),
    numeric( B ),
    X is A/B.
 
-simp( A^B, X ) :-       /* do evaluation if both are numbers  */
+/* Potęgowanie stałych */
+simplify( A^B, X ) :-
    numeric( A ),
    numeric( B ),
    X is A^B.
 
-simp( W+X, Q ):-        /* catchall */
-   simp( W, Y ),
-   simp( X, Z ),
-   ( W \== Y ; X \== Z ), /* ensure some simplification has taken place */
-   simp( Y+Z, Q ).
+/* Suma wyrażeń */
+/* Warunek W \== Y zapewnia że ten predykat nie "zajmie" podstawienia nic przy tym nie robiąc */
+simplify( W+X, Q ):-
+   simplify( W, Y ),
+   simplify( X, Z ),
+   ( W \== Y ; X \== Z ),
+   simplify( Y+Z, Q ).
 
-simp( W-X, Q ):-        /* catchall */
-   simp( W, Y ),
-   simp( X, Z ),
-   ( W \== Y ; X \== Z ), /* ensure some simplification has taken place */
-   simp( Y-Z, Q ).
+/* Różnica wyrażeń */
+simplify( W-X, Q ):-
+   simplify( W, Y ),
+   simplify( X, Z ),
+   ( W \== Y ; X \== Z ),
+   simplify( Y-Z, Q ).
 
-simp( W*X, Q ):-        /* catchall */
-   simp( W, Y ),
-   simp( X, Z ),
-   ( W \== Y  ; X \== Z ), /* ensure some simplificaion has taken place */
-   simp( Y*Z, Q ).
+/* Iloczyn wyrażeń */
+simplify( W*X, Q ):-
+   simplify( W, Y ),
+   simplify( X, Z ),
+   ( W \== Y  ; X \== Z ),
+   simplify( Y*Z, Q ).
 
-simp( A/B, C ) :-
-   simp( A, X ),
-   simp( B, Y ),
+/* Iloraz wyrażeń */
+simplify( A/B, C ) :-
+   simplify( A, X ),
+   simplify( B, Y ),
    ( A \== X ; B \== Y ),
-   simp( X/Y, C ).
+   simplify( X/Y, C ).
 
-simp( X^A, C ) :-
-   simp( A, B ),
+/* Potęgowanie wyrażeń */
+simplify( X^A, C ) :-
+   simplify( A, B ),
    A \== B,
-   simp( X^B, C).
+   simplify( X^B, C).
 
-simp( X, X ).           /* if all else fails... */              
+/* Zabezpieczenie żeby "dziwne" wyrażenia nie spowodowały błędów */
+simplify( X, X ).           
+
+/* Liczenie wartości: wyrażenie, symbol, wartość symbolu, wynik  */
+calc( X, A, a, B, b, C, c, result ) :-
+    calc( X, A, a, r1 ),
+    calc( r1, B, b, r2 ),
+    calc( r2, C, c, result ).
+calc( X, A, a, B, b, result ) :-
+    calc( X, A, a, r1 ),
+    calc( r1, B, b, result ).
+
+/* Wartość stałej jest znana */
+calc( X, _A, a, result ) :-
+    numeric(X),
+    result is X.
+/* Wartość symbolu o danej wartości */
+calc( X, A, a, result ) :-
+    X = A,
+    result is a.
 
 
+/* Atom jest liczbą kiedy jest liczbą całkowitą lub zmiennoprzecinkową */
 numeric(A) :- integer(A).
-
 numeric(A) :- float(A).
